d0c748563c51610fd47e087cf81a2f66471b8775
resteve <resteve@zikzakmedia.com>
2016-01-19 09:38:05 +0100

    Open SAO in browser

--- trytontasks_sao/__init__.py
+++ trytontasks_sao/__init__.py
@@ -1,8 +1,10 @@
 #This file is part of trytontasks_sao. The COPYRIGHT file at the top level of
 #this repository contains the full copyright notices and license terms.
 import os
+import webbrowser
 from invoke import task
 from blessings import Terminal
+from trytontasks_modules import read_config_file
 
 t = Terminal()
 
@@ -30,3 +32,18 @@ def locales():
     os.system('grunt po2json')
 
     print t.bold('Done')
+
+@task
+def open(server):
+    'Open SAO in browser'
+    Servers = read_config_file('servers.cfg', type='servers')
+    servers = Servers.sections()
+    servers.sort()
+
+    if not server in servers:
+        print 'Not found %s' % server
+        return
+
+    url = Servers.get(server, 'sao')
+    if url:
+        webbrowser.open(url, new=1)
